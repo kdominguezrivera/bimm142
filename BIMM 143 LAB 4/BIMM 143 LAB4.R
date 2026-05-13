a4a118fc95a4a8b72396c0434bbdafbdf56c2ab3
kdominguezrivera <kdominguezrivera@ucsd.edu>
2026-05-13 10:59:10 -0700

    Uploading

---
+++ BIMM 143 LAB 4/BIMM 143 LAB4.R
@@ -0,0 +1,11 @@
+x<- 1:50
+plot(x, sin(x))
+?plot 
+plot (x, sin(x), type="l")
+plot(x, sin(x), type="l", col="BLUE")
+plot(x, sin(x), type="l", col="BLUE", lwd=3)
+1:50
+?seq
+seq(1, 50, by=0.1)
+x<- seq(1,50,by=0.1)
+plot(x, sin(x), type="l", col="BLUE", lwd=3)
